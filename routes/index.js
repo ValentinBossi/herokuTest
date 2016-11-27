e40f60a7b0e934568971020e30c34435a41bc422
Valentin Bossi <bossival@students.zhaw.ch>
2016-11-27 21:10:16 +0100

    intention doesnt work

--- routes/index.js
+++ routes/index.js
@@ -3,24 +3,8 @@ var router = express.Router();
 
 var db = require('../queries');
 
-function getAllPuppies(req, res, next) {
-    db.any('select * from pups')
-        .then(function(data) {
-            res.status(200)
-                .json({
-                    status: 'success',
-                    data: data,
-                    message: 'Retrieved ALL puppies'
-                });
-            console.log(data[0]);
-        })
-        .catch(function(err) {
-            return next(err);
-        });
-}
 
-
-router.get('/api/puppies', getAllPuppies);
+router.get('/api/puppies', db.getAllPuppies);
 router.get('/api/puppies/:id', db.getSinglePuppy);
 router.post('/api/puppies', db.createPuppy);
 router.put('/api/puppies/:id', db.updatePuppy);
